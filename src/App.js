aaa17845f7b519d72c47a1fa96d96089e8362efd
Shyngys <zhakelov.shyngys@gmail.com>
2020-07-30 16:42:16 +0500

    text test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+var questions = ["1+1=?", "2+2=?","3+3=?","4+4=?"];
+var answers = ["2", "4","6","8"];
+var i=getRandomInt(questions.length);
+var answer;
 
 function App() {
+  const [text, setText] = React.useState("");
+  const [text2,setText2]=React.useState(questions[i]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{text2}</div>
+      <input
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          setText("");
+        }}
+      >
+        Reset
+      </button>
+
+      <button
+        onClick={() => {
+          if (text===answers[i]) {
+          i=getRandomInt(questions.length);
+          setText2(questions[i]);
+          answer="Correct";
+        }
+          else {
+          i=getRandomInt(questions.length);
+          setText2(questions[i]);
+          answer="Incorrect";
+        }; 
+
+        alert(answer);
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }

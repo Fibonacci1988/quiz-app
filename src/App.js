5d067e42c68a1a352e6f735b5dc4c70b99f00669
Shyngys <zhakelov.shyngys@gmail.com>
2020-08-10 06:01:10 +0500

    homework 3

--- src/App.js
+++ src/App.js
@@ -7,17 +7,110 @@ function getRandomInt(max) {
 }
 
 
-var questions = ["1+1=?", "2+2=?","3+3=?","4+4=?"];
-var answers = ["2", "4","6","8"];
-var i=getRandomInt(questions.length);
-var answer;
+const questionsOpenEnded = ["1+1=?", "2+2=?","3+3=?","4+4=?"];
+const answers = ["2", "4","6","8"];
+
+const questionsMultipleChoice= [
+  {id: 1,text: '1+0=?',choices: [
+      {
+        id: 'a',
+        text: '1'
+      },
+      {
+        id: 'b',
+        text: '2'
+      },
+      {
+        id: 'c',
+        text: '3'
+      },
+      {
+        id: 'd',
+        text: '4'
+      }
+    ],
+    correct: 'a'},
+
+    {id: 2,text: '1+1=?',choices: [
+      {
+        id: 'a',
+        text: '1'
+      },
+      {
+        id: 'b',
+        text: '2'
+      },
+      {
+        id: 'c',
+        text: '3'
+      },
+      {
+        id: 'd',
+        text: '4'
+      }
+    ],
+    correct: 'b'},
+
+    {id: 3,text: '1+2=?',choices: [
+      {
+        id: 'a',
+        text: '1'
+      },
+      {
+        id: 'b',
+        text: '2'
+      },
+      {
+        id: 'c',
+        text: '3'
+      },
+      {
+        id: 'd',
+        text: '4'
+      }
+    ],
+    correct: 'c'},
+
+    {id: 4,text: '1+3=?',choices: [
+      {
+        id: 'a',
+        text: '1'
+      },
+      {
+        id: 'b',
+        text: '2'
+      },
+      {
+        id: 'c',
+        text: '3'
+      },
+      {
+        id: 'd',
+        text: '4'
+      }
+    ],
+    correct: 'd'}
+
+  
+  ]
+ 
+
+let i=getRandomInt(questionsOpenEnded.length);
+let j=getRandomInt(questionsMultipleChoice.length);
+
+
+
 
 function App() {
   const [text, setText] = React.useState("");
-  const [text2,setText2]=React.useState(questions[i]);
+  const [question,setQuestion]=React.useState(questionsOpenEnded[i]);
+  const [question2,setQuestion2]=React.useState(questionsMultipleChoice[j].text)
+  const [questionType,setQuestionType]=React.useState(getRandomInt(2));
+  if (questionType===0) {
+  
   return (
     <div>
-      <div>{text2}</div>
+    <div>{question}</div>
       <input
         value={text}
         onChange={(event) => {
@@ -34,24 +127,122 @@ function App() {
 
       <button
         onClick={() => {
+          
           if (text===answers[i]) {
-          i=getRandomInt(questions.length);
-          setText2(questions[i]);
-          answer="Correct";
+          alert("Correct");
         }
           else {
-          i=getRandomInt(questions.length);
-          setText2(questions[i]);
-          answer="Incorrect";
+          alert("Incorrect");
         }; 
-
-        alert(answer);
+        i=getRandomInt(questionsOpenEnded.length);
+        setQuestion(questionsOpenEnded[i]);
+        setQuestionType(getRandomInt(2));
+      
+        
         }}
       >
         Submit
       </button>
+
     </div>
-  );
+  ); }
+
+  else {return (
+
+
+    <div>
+   <div>{question2}</div>
+
+    <div>
+   <button
+        onClick={() => {
+          
+          if (questionsMultipleChoice[j].choices[0].id===questionsMultipleChoice[j].correct) {
+          alert("Correct");
+        }
+          else {
+          alert("Incorrect");
+        }; 
+        j=getRandomInt(questionsMultipleChoice.length);
+        setQuestion2(questionsMultipleChoice[j].text);
+        setQuestionType(getRandomInt(2));
+      
+        
+        }}
+      >
+        {questionsMultipleChoice[j].choices[0].text}
+      </button>
+      </div>
+
+      <div>
+   <button
+        onClick={() => {
+          
+          if (questionsMultipleChoice[j].choices[1].id===questionsMultipleChoice[j].correct) {
+          alert("Correct");
+        }
+          else {
+          alert("Incorrect");
+        }; 
+        j=getRandomInt(questionsMultipleChoice.length);
+        setQuestion2(questionsMultipleChoice[j].text);
+        setQuestionType(getRandomInt(2));
+      
+        
+        }}
+      >
+        {questionsMultipleChoice[j].choices[1].text}
+      </button>
+      </div>
+
+      <div>
+   <button
+        onClick={() => {
+          
+          if (questionsMultipleChoice[j].choices[2].id===questionsMultipleChoice[j].correct) {
+          alert("Correct");
+        }
+          else {
+          alert("Incorrect");
+        }; 
+        j=getRandomInt(questionsMultipleChoice.length);
+        setQuestion2(questionsMultipleChoice[j].text);
+        setQuestionType(getRandomInt(2));
+      
+        
+        }}
+      >
+        {questionsMultipleChoice[j].choices[2].text}
+      </button>
+      </div>
+
+      <div>
+   <button
+        onClick={() => {
+          
+          if (questionsMultipleChoice[j].choices[3].id===questionsMultipleChoice[j].correct) {
+          alert("Correct");
+        }
+          else {
+          alert("Incorrect");
+        }; 
+        j=getRandomInt(questionsMultipleChoice.length);
+        setQuestion2(questionsMultipleChoice[j].text);
+        setQuestionType(getRandomInt(2));
+      
+        
+        }}
+      >
+        {questionsMultipleChoice[j].choices[3].text}
+      </button>
+      </div>
+
+
+    </div>
+
+  )
+  
+  }
 }
 
 export default App;
